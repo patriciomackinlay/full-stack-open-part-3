eb9f44257e6d64de7e4812867bcc9d756a709905
patriciomackinlay <141454324+patriciomackinlay@users.noreply.github.com>
2023-12-06 17:52:11 -0300

    Complete 3.6

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,5 +1,8 @@
+const e = require("express")
 const express = require("express")
 const app = express()
+
+app.use(express.json())
 
 let persons = [
     { 
@@ -52,7 +55,36 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
+app.post("/api/persons", (request, response) => {
+    const body = request.body
+    const id = Math.round(Math.random() *10** 10)
+    if(!body.name) {
+        return response.status(400).json({
+            error:"name missing"
+        })
+    }
+    if(!body.number) {
+        return response.status(400).json({
+            error:"number missing"
+        })
+    }
+    if(persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error:"name must be unique"
+        })
+    }
+
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, ()=> {
-    console.log("server running on port ", PORT)
 })
